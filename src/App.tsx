f8421ed7268f1cc866a46edc5e34aa583235d545
chihiro <chieeie@gmail.com>
2025-03-04 18:59:19 +0900

    supabaseとloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import { Button } from "@chakra-ui/react";
+import { Records } from "./domain/records";
+import { getAllRecords } from "../utils/supabaseFunctions";
+
 function App() {
+  const [records, setRecords] = useState<Records[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const getRecords = async () => {
+      const data = await getAllRecords();
+      setRecords(data);
+      setLoading(false);
+    };
+    getRecords();
+  }, []);
   return (
     <>
       <div>
         <h1>Vite</h1>
+        <Button>ボタン</Button>
+        <ul>
+          {loading ? (
+            <p>loading...</p>
+          ) : (
+            records.map((record) => (
+              <li key={record.id}>
+                {record.title} {record.time}
+              </li>
+            ))
+          )}
+        </ul>
       </div>
     </>
   );
---
+++ utils/supabaseFunctions.tsx
@@ -0,0 +1,28 @@
+import { Records } from "../src/domain/records";
+import { supabase } from "./supabase";
+
+export async function getAllRecords():Promise<Records[]> {
+  const { data, error } = await supabase.from("study-record").select("*");
+  if (error) {
+    console.error("Error:", error.message);
+  }
+  if (!data) {
+    return [];
+  }
+
+  return data ?? [];
+}
+
+export const addRecords = async (title, time) => {
+  const { data, error } = await supabase
+    .from("study-record")
+    .insert({ title: title, time: time });
+  if (error) {
+    console.error("Error:", error.message);
+  }
+  return data;
+};
+
+export const deleteRecords = async (id) => {
+  await supabase.from("study-record").delete().eq("id", id);
+};
