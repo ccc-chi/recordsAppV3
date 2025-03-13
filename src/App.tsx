5404c57562160cdac9a6d49eb3227fe097905fd4
chihiro <chieeie@gmail.com>
2025-03-13 18:50:47 +0900

    supabaseに登録・バリデーション途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { useEffect, useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+
 import { Records } from "./domain/records";
-import { getAllRecords } from "../utils/supabaseFunctions";
+import { addRecords, getAllRecords } from "../utils/supabaseFunctions";
+
+interface IFormInput {
+  title: string;
+  time: number;
+}
 
 function App() {
   const [records, setRecords] = useState<Records[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  //-- 学習データをフェッチ
   useEffect(() => {
     const getRecords = async () => {
       const data = await getAllRecords();
@@ -14,11 +34,93 @@ function App() {
     };
     getRecords();
   }, []);
+
+  //-- 学習記録を登録
+  // const [title, setTitle] = useState<string>("");
+  // const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) =>
+  //   setTitle(e.target.value);
+  // const [time, setTime] = useState<number>(0);
+  // useEffect(() => {}, []);
+  // const onChangeTime = (e: React.ChangeEvent<HTMLInputElement>) =>
+  //   setTime(Number(e.target.value));
+  // const onClickAdd = async () => {
+  //   // supabaseに追加
+  //   await addRecords(title, time);
+  //   // リストに追加
+  //   const records = await getAllRecords();
+  //   setRecords(records);
+  //   // inputをリセット
+  //   setTitle("");
+  //   setTime(0);
+  // };
+
+  //-- バリデーション
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    console.log(data);
+    await addRecords(data.title, data.time);
+    // リストに追加
+    const records = await getAllRecords();
+    setRecords(records);
+    // inputをリセット
+    // setTitle("");
+    // setTime(0);
+  };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <div>
         <h1>Vite</h1>
-        <Button>ボタン</Button>
+        <Button onClick={onOpen}>登録</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>STUDY RECORD</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <p>学習記録を登録する</p>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Box>
+                  <p>学習内容：</p>
+                  <Input
+                    type="text"
+                    {...register("title", { required: "必須入力" })}
+                    placeholder="学習内容を入力してください"
+                  />
+                  {errors?.title?.type === "required" && (
+                    <p>内容の入力は必須です</p>
+                  )}
+                </Box>
+                <Box>
+                  <p>学習時間：</p>
+                  <Input
+                    type="number"
+                    {...register("time", {
+                      required: "必須入力",
+                      min: 18,
+                      max: 99,
+                    })}
+                  />
+                  {errors?.time?.type === "required" && (
+                    <p>時間の入力は必須です</p>
+                  )}
+                </Box>
+                <Button type="submit">登録</Button>
+              </form>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
         <ul>
           {loading ? (
             <p>loading...</p>
--- utils/supabaseFunctions.tsx
+++ utils/supabaseFunctions.tsx
@@ -1,7 +1,7 @@
 import { Records } from "../src/domain/records";
 import { supabase } from "./supabase";
 
-export async function getAllRecords():Promise<Records[]> {
+export const getAllRecords = async (): Promise<Records[]> => {
   const { data, error } = await supabase.from("study-record").select("*");
   if (error) {
     console.error("Error:", error.message);
@@ -11,4 +11,20 @@ export async function getAllRecords():Promise<Records[]> {
   }
 
   return data ?? [];
-}
+};
+
+export const addRecords = async (
+  title: string,
+  time: number
+): Promise<Records[]> => {
+  const { data, error } = await supabase
+    .from("study-record")
+    .insert({ title: title, time: time });
+  if (error) {
+    console.error("Error:", error.message);
+  }
+  if (!data) {
+    return [];
+  }
+  return data;
+};
