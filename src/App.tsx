aff911585cd23ef095c1826f2feadf9555a9ab45
chihiro <chieeie@gmail.com>
2025-03-25 13:16:22 +0900

    モーダルは使い回し

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,23 @@ function App() {
     formState: { errors },
   } = useForm<IFormInput>();
 
+  //-- 削除
+  const onClickDeleteRecords = async (id: string) => {
+    await deleteRecords(id);
+    const records = await getAllRecords();
+    setRecords(records);
+  };
+
+  //-- モーダル
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isAdd, setIsAdd] = useState<boolean>(true);
+
   //-- 登録
+  const onClickAddMode = () => {
+    reset({ id: undefined, title: "", time: 0 });
+    setIsAdd(true);
+    onOpen();
+  };
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     console.log(data);
     // データを追加
@@ -58,24 +74,17 @@ function App() {
     // inputをリセット
     reset();
     // モーダルを閉じる
-    addModal.onClose();
-  };
-
-  //-- 削除
-  const onClickDeleteRecords = async (id: string) => {
-    await deleteRecords(id);
-    const records = await getAllRecords();
-    setRecords(records);
+    onClose();
   };
 
   //-- 編集
   const onClickEditRecords = async (id: string) => {
+    setIsAdd(false);
     const record = records.find((record) => record.id === id);
     if (!record) return;
     reset({ id: record.id, title: record.title, time: record.time });
-    editModal.onOpen();
+    onOpen();
   };
-
   const onEditSubmit: SubmitHandler<IFormInput> = async (data) => {
     console.log(data);
     if (!data.id) {
@@ -88,28 +97,28 @@ function App() {
     // inputをリセット
     reset({ id: undefined, title: "", time: 0 });
     // モーダルを閉じる
-    editModal.onClose();
+    onClose();
   };
-
-  //-- モーダル
-  const addModal = useDisclosure();
-  const editModal = useDisclosure();
 
   return (
     <>
       <div>
         <h1 data-testid="appTitle">学習記録アプリ</h1>
-        <Button onClick={addModal.onOpen} data-testid="openModalButton">
+        <Button onClick={onClickAddMode} data-testid="openModalButton">
           記録を追加する
         </Button>
-        <Modal isOpen={addModal.isOpen} onClose={addModal.onClose}>
+        <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader data-testid="modalTitle">新規登録</ModalHeader>
+            {isAdd ? (
+              <ModalHeader data-testid="modalTitle">記録追加</ModalHeader>
+            ) : (
+              <ModalHeader data-testid="modalEditTitle">記録編集</ModalHeader>
+            )}
             <ModalCloseButton />
             <ModalBody>
-              <p>学習記録を登録する</p>
-              <form onSubmit={handleSubmit(onSubmit)}>
+              {isAdd ? <p>学習記録を登録する</p> : <p>学習記録を編集する</p>}
+              <form onSubmit={handleSubmit(isAdd ? onSubmit : onEditSubmit)}>
                 <Box>
                   <p>学習内容：</p>
                   <Input
@@ -147,73 +156,15 @@ function App() {
                     </p>
                   )}
                 </Box>
-                <Button type="submit" data-testid="addButton">
-                  登録
-                </Button>
-              </form>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={addModal.onClose}>
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-
-        <Modal isOpen={editModal.isOpen} onClose={editModal.onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader data-testid="modalEditTitle">記録編集</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <p>学習記録を編集する</p>
-              <form onSubmit={handleSubmit(onEditSubmit)}>
-                <Box>
-                  <p>学習内容：</p>
-                  <Input
-                    type="text"
-                    data-testid="titleEditInput"
-                    {...register("title", {
-                      required: "内容の入力は必須です",
-                    })}
-                    placeholder="学習内容を入力してください"
-                  />
-                  {errors.title && (
-                    <span data-testid="titleEditError" style={{ color: "red" }}>
-                      {errors.title.message}
-                    </span>
-                  )}
-                </Box>
-                <Box>
-                  <p>学習時間：</p>
-                  <Input
-                    type="number"
-                    data-testid="timeEditInput"
-                    {...register("time", {
-                      required: true,
-                      min: 1,
-                      max: 99,
-                      valueAsNumber: true,
-                    })}
-                  />
-                  {errors?.time?.type === "required" && (
-                    <p
-                      data-testid="timeEditErrorRequired"
-                      style={{ color: "red" }}
-                    >
-                      時間の入力は必須です
-                    </p>
-                  )}
-                  {errors?.time?.type === "min" && (
-                    <p data-testid="timeEditErrorMin" style={{ color: "red" }}>
-                      時間は0以上で入力してください
-                    </p>
-                  )}
-                </Box>
-                <Button type="submit" data-testid="addEditButton">
-                  編集内容を登録
-                </Button>
+                {isAdd ? (
+                  <Button type="submit" data-testid="addButton">
+                    登録
+                  </Button>
+                ) : (
+                  <Button type="submit" data-testid="addEditButton">
+                    編集内容を登録
+                  </Button>
+                )}
               </form>
             </ModalBody>
 
@@ -222,8 +173,8 @@ function App() {
                 colorScheme="blue"
                 mr={3}
                 onClick={() => {
-                  reset({ id: undefined, title: "", time: 0 });
-                  editModal.onClose();
+                  onClose();
+                  // reset({ id: undefined, title: "", time: 0 });
                 }}
               >
                 Close
